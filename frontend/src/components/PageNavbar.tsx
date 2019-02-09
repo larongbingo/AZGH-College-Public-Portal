add859557f73c0be86733d3466c79c4d3baaf7ea
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-02-09 23:50:05 +0800

    Added navbar

---
+++ frontend/src/components/PageNavbar.tsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { Container, Image, Nav, Navbar } from "react-bootstrap";
+
+import schoolLogo from "./school-logo.png";
+
+export class PageNavbar extends Component {
+  public render() {
+    return (
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">
+            <Image 
+              src={schoolLogo} 
+              width="30px"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          {
+            // @ts-ignore
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav>
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/courses">Courses</Nav.Link>
+                <Nav.Link href="/about">About Us</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          }
+        </Container>
+      </Navbar>
+    );
+  }
+}
+
+export default PageNavbar;
