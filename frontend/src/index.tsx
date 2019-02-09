36f473c6ef49d7f70c92f4e4c0a35c980a457f7b
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-02-09 23:49:30 +0800

    Added rendering of bootstrap css

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// tslint:disable:no-submodule-imports
+// tslint:disable-next-line:no-implicit-dependencies
+import "bootstrap/dist/css/bootstrap.css";
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
