8d71a4ec13ff49a8f0a965949adfadc5913a0cfb
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-02-09 23:48:56 +0800

    Modified root react component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
+
+import PageNavbar from "./components/PageNavbar";
+import { routes as Routes } from "./Routes";
 
 class App extends Component {
-  render() {
+  public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <>
+          <PageNavbar/>
+          <Routes/>
+        </>
+      </BrowserRouter>
     );
   }
 }
