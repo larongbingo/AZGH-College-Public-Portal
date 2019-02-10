a09ce8727fe9ce89bae5eb67e8a3760ec2398f93
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-02-10 20:43:35 +0800

    Added routes of about and courses page

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,10 +1,14 @@
 import React, { FunctionComponent } from "react";
 import { Route, Switch } from "react-router-dom";
 
+import about from "./pages/about";
+import courses from "./pages/courses";
 import { index } from "./pages/index";
 
 export const routes: FunctionComponent = () => (
   <Switch>
+    <Route exact path="/courses" component={courses} />
+    <Route exact path="/about" component={about} />
     <Route exact path="/" component={index} />
   </Switch>
 );
