472294618b7f51b08c0c69b2daf4b832f0bb5b54
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-02-12 20:00:49 +0800

    Added login and register to routing

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -4,9 +4,13 @@ import { Route, Switch } from "react-router-dom";
 import about from "./pages/about";
 import courses from "./pages/courses";
 import { index } from "./pages/index";
+import login from "./pages/login";
+import register from "./pages/register";
 
 export const routes: FunctionComponent = () => (
   <Switch>
+    <Route exact path="/login" component={login} />
+    <Route exact path="/register" component={register} />
     <Route exact path="/courses" component={courses} />
     <Route exact path="/about" component={about} />
     <Route exact path="/" component={index} />
