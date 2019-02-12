b5105fc231a939df1c216599a93583b12483ded6
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-02-12 20:01:25 +0800

    Added register page

---
+++ frontend/src/pages/register/index.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent } from "react";
+import { Container, Form, FormControl } from "react-bootstrap";
+
+export const register: FunctionComponent = () => (
+  <Container>
+    <h1>Registration</h1>
+    <Form>
+      
+    </Form>
+    <a href="/login">Already have an account? Log In here</a>
+  </Container>
+);
+
+export default register;
