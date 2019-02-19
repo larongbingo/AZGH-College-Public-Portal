2c2e15c33b861b78c94cf359a17c6ec191c33e35
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-02-19 14:54:24 +0800

    Added an example form of config.ts

---
+++ backend/app/config.example.ts
@@ -0,0 +1,9 @@
+/**
+ * CREATE A COPY OF THIS FILE AND RENAME IT AS "config.ts"
+ */
+
+export const DATABASE = {
+  database: "The database where we will store the data",
+  username: "username of the mysql account",
+  password: "password of the mysql account"
+};
