d44fa2fb20ff60b9c300847ea1d20cf4dc64ef39
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-02-21 15:04:16 +0800

    Added routing for profile and 404

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -5,15 +5,17 @@ import about from "./pages/about";
 import courses from "./pages/courses";
 import { index } from "./pages/index";
 import login from "./pages/login";
-import register from "./pages/register";
+import page404 from "./pages/page404";
+import profile from "./pages/profile";
 
 export const routes: FunctionComponent = () => (
   <Switch>
+    <Route exact path="/profile" component={profile} />
     <Route exact path="/login" component={login} />
-    <Route exact path="/register" component={register} />
     <Route exact path="/courses" component={courses} />
     <Route exact path="/about" component={about} />
     <Route exact path="/" component={index} />
+    <Route component={page404} />
   </Switch>
 );
 
