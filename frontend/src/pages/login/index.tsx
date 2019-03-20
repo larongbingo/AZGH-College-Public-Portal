ca255bb505f04956daa4915d75a775fedb623aba
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-03-20 19:28:30 +0800

    feat: added API to login page

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,23 +1,115 @@
-import React, { FunctionComponent } from "react";
-import { Container, Form, FormGroup } from "react-bootstrap";
+import React, { Component } from "react";
+import { Alert, Container, Form, FormGroup } from "react-bootstrap";
 
-export const login: FunctionComponent = () => (
-  <Container>
-    <Form>
-      <Form.Group>
-        <Form.Label>Email Address</Form.Label>
-        <Form.Control name="email" placeholder="Email Address" />
-      </Form.Group>
+import { ENDPOINT, SESSION_KEY } from "../../config";
 
-      <FormGroup>
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </FormGroup>
+export class LogIn extends Component<{}, ILogInStates> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      isLogInFailed: false
+    };
 
-      <Form.Control type="submit" />
-    </Form>
-    <a href="/register">Don't have an account? Register here</a>
-  </Container>
-);
+    this._usernameKeyPressHandler = this._usernameKeyPressHandler.bind(this);
+    this._passwordKeyPressHandler = this._passwordKeyPressHandler.bind(this);
+    this._logInRequest = this._logInRequest.bind(this);
+    this._onEnterKeyPress = this._onEnterKeyPress.bind(this);
+  }
 
-export default login;
+  public render() {
+    return (
+      <Container>
+        {
+          // @ts-ignore
+          this.state.isLogInFailed ?
+          <Alert dismissible variant="danger">
+            <Alert.Heading>Invalid Credentials</Alert.Heading>
+          </Alert> : 
+          null
+        }
+
+        <Form id="form-control" action={ENDPOINT + "/apis/login"} encType="multipart/form-data" method="post">
+          <Form.Group>
+            <Form.Label>Username</Form.Label>
+            <Form.Control 
+              name="username" 
+              type="text"
+              placeholder="Username"
+              value={this.state.username}
+              onChange={this._usernameKeyPressHandler}
+            />
+          </Form.Group>
+    
+          <FormGroup>
+            <Form.Label>Password</Form.Label>
+            <Form.Control 
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={this.state.password} 
+              onChange={this._passwordKeyPressHandler}
+            />
+          </FormGroup>
+    
+          <Form.Control type="button" value="Log In" onClick={this._logInRequest} />
+        </Form>
+
+        <a href="/register">Don't have an account? Register here</a>
+      </Container>
+    );
+  }
+
+  private _usernameKeyPressHandler(e: any) {
+    this.setState({username: e.target.value});
+  }
+
+  private _passwordKeyPressHandler(e: any) {
+    this.setState({password: e.target.value});
+  }
+
+  private _onEnterKeyPress(e: any) {
+    e.preventDefault();
+    if(e.key === "Enter") {
+      this._logInRequest();
+    }
+  }
+
+  private _logInRequest() {
+    let formData = new FormData(document.getElementById("form-control") as HTMLFormElement);
+
+    fetch(ENDPOINT + "/apis/login", {
+      method: "POST",
+      mode: "cors",
+      body: formData
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      // Check if the session token is stored
+      if(!data.payload.token) {
+        // Alert user that the login failed, probably of incorrect credentials
+        this.setState({ isLogInFailed: true });
+        return;
+      }
+      
+      // Store session token
+      sessionStorage.setItem(SESSION_KEY, data.payload.token);
+
+      this.setState({ isLogInFailed: false });
+
+      // Redirect to app
+      // @ts-ignore
+      this.props.history.push("/app");
+    })
+    .catch((err) => console.error(err));
+  }
+}
+
+interface ILogInStates {
+  username: string;
+  password: string;
+  isLogInFailed: boolean;
+}
+
+export default LogIn;
