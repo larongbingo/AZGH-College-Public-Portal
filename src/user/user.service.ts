aa47fe24db44d364047f3fb2e02f3fefde959492
Renz Pagulayan <rcyapagulayan@gmail.com>
2019-05-03 19:05:34 +0800

    feat: user service

---
+++ src/user/user.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@nestjs/common";
+import { Op } from "sequelize";
+import { IFindOptions } from "sequelize-typescript";
+
+import { User } from "../database/models/User.entity";
+
+@Injectable()
+export class UserService {
+  public async findOneByToken(token: string) {
+    return User.findOne({where: {token: {[Op.eq]: token}}});
+  }
+
+  public async findOne(options?: IFindOptions<User>) {
+    return User.findOne(options);
+  }
+}
