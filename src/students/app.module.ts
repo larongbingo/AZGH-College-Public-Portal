d1da91e3b80e8b4b4907c5ab8e55ff5937c98c60
Renz Pagulayan <rcyapagulayan@gmail.com>
2019-05-04 21:32:07 +0800

    refactor: use separate entrypoints for each server

---
+++ src/students/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common";
+
+import { UserModule } from "../shared/user/user.module";
+import { DatabaseModule } from "../shared/database/database.module";
+import { AuthModule } from "../shared/auth/auth.module";
+
+@Module({
+  imports: [DatabaseModule, UserModule, AuthModule],
+})
+export class AppModule {}
