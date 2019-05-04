c054dce8ff298aaca60f7b84cebae4ebcb9e4f38
Renz Pagulayan <rcyapagulayan@gmail.com>
2019-05-04 20:10:48 +0800

    feat(userService): create and findAll db ops

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { Op } from "sequelize";
-import { IFindOptions } from "sequelize-typescript";
+import { IFindOptions, ICreateOptions } from "sequelize-typescript";
+import { FilteredModelAttributes } from "sequelize-typescript/lib/models/Model";
 
 import { User } from "../database/models/User.entity";
 
@@ -12,5 +13,13 @@ export class UserService {
 
   public async findOne(options?: IFindOptions<User>) {
     return User.findOne(options);
+  }
+
+  public async findAll(options?: IFindOptions<User>) {
+    return User.findAll(options);
+  }
+
+  public async create(values: FilteredModelAttributes<User>, options?: ICreateOptions) {
+    return User.create(values, options);
   }
 }
