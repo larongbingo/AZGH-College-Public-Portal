a971068cecd56b2ab2229acba1941261bafe5e98
Renz Pagulayan <rcyapagulayan@gmail.com>
2019-05-04 20:03:16 +0800

    feat(appModule): added AuthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from "@nestjs/common";
 
 import { UserModule } from "./user/user.module";
 import { DatabaseModule } from "./database/database.module";
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
-  imports: [DatabaseModule, UserModule],
+  imports: [DatabaseModule, UserModule, AuthModule],
 })
 export class AppModule {}
