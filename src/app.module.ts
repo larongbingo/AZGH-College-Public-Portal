41b8dbc6d595cd9e5f701100b38edb5bfcfac0e3
Renz Pagulayan <rcyapagulayan@gmail.com>
2019-05-04 20:22:55 +0800

    refactor: moved common between 2 servers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from "@nestjs/common";
 
-import { UserModule } from "./user/user.module";
-import { DatabaseModule } from "./database/database.module";
-import { AuthModule } from "./auth/auth.module";
+import { UserModule } from "./shared/user/user.module";
+import { DatabaseModule } from "./shared/database/database.module";
+import { AuthModule } from "./shared/auth/auth.module";
 
 @Module({
   imports: [DatabaseModule, UserModule, AuthModule],
