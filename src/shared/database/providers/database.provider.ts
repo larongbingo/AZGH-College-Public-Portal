1d14a67a6ad39920bf89333d9fcdd275342fd31e
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-05-17 15:33:13 +0800

    refactor: moved sequelize instance to new file

--- src/shared/database/providers/database.provider.ts
+++ src/shared/database/providers/database.provider.ts
@@ -1,25 +1,10 @@
 import { Provider } from "@nestjs/common";
-import { Sequelize, ISequelizeConfig } from "sequelize-typescript";
 
-import { development, production } from "../../config/database.config";
-import { addModels } from "../libs/add.models";
+import { sequelize } from "../sequelize.instance";
 
 export const SEQUELIZE_REPOSITORY = "SEQUELIZE_REPOSITORY";
 
 export const databaseProvider: Provider = {
   provide: SEQUELIZE_REPOSITORY,
-  useFactory: async () => {
-    let config: ISequelizeConfig;
-    if (process.env.NODE_ENV === "production") {
-      config = production;
-    } else {
-      config = development;
-    }
-
-    const sequelize = new Sequelize({
-      ...(config as ISequelizeConfig),
-    });
-    addModels(sequelize);
-    return sequelize;
-  },
+  useFactory: async () => sequelize,
 };
