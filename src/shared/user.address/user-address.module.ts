fdaa28e9ee57cfcd3124a424d32b62c7a51fb596
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-05-17 19:33:35 +0800

    feat(user-address): added controller to module

--- src/shared/user.address/user-address.module.ts
+++ src/shared/user.address/user-address.module.ts
@@ -3,10 +3,12 @@ import { Module } from "@nestjs/common";
 import { UserAddressProvider } from "./user-address.provider";
 import { UserAddressService } from "./user-address.service";
 import { UserModule } from "../user/user.module";
+import { UserAddressController } from "./user-address.controller";
 
 @Module({
   imports: [UserModule],
   providers: [UserAddressProvider, UserAddressService],
+  controllers: [UserAddressController],
   exports: [UserAddressService],
 })
 export class UserAddressModule {}
