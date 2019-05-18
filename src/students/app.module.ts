942696db95531a69432af15508eb617a5cfd3099
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-05-18 20:40:05 +0800

    feat: added userAddress to root module

--- src/students/app.module.ts
+++ src/students/app.module.ts
@@ -3,8 +3,14 @@ import { Module } from "@nestjs/common";
 import { UserModule } from "../shared/user/user.module";
 import { DatabaseModule } from "../shared/database/database.module";
 import { AuthModule } from "../shared/auth/auth.module";
+import { UserAddressModule } from "../shared/user.address/user-address.module";
 
 @Module({
-  imports: [DatabaseModule, UserModule, AuthModule],
+  imports: [
+    DatabaseModule,
+    UserModule,
+    UserAddressModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
