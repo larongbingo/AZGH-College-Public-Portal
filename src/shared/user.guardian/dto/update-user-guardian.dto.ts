55c3be1517e595d2a5efc1566008857f5663f31f
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-05-19 19:28:52 +0800

    feat(user-guardian): controller

---
+++ src/shared/user.guardian/dto/update-user-guardian.dto.ts
@@ -0,0 +1,9 @@
+
+export class UpdateUserGuardianDto  {
+  public readonly firstName?: string;
+  public readonly middleName?: string;
+  public readonly lastName?: string;
+  public readonly mobileNumber?: string;
+  public readonly emailAddress?: string;
+  public readonly occupation?: string;
+}
--- src/shared/user.guardian/user-guardian.module.ts
+++ src/shared/user.guardian/user-guardian.module.ts
@@ -2,9 +2,11 @@ import { Module } from "@nestjs/common";
 
 import { UserGuardianProvider } from "./user-guardian.provider";
 import { UserGuardianService } from "./user-guardian.service";
+import { UserGuardianController } from "./user-guardian.controller";
 
 @Module({
   providers: [UserGuardianProvider, UserGuardianService],
   exports: [UserGuardianService],
+  controllers: [UserGuardianController],
 })
 export class UserGuardianModule {}
