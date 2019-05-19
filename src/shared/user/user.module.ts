9720fd5e8f9a5dbfdd9396253add7d1b93f588ce
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-05-19 12:42:12 +0800

    feat(user-module): imports all user details module

--- src/shared/user/user.module.ts
+++ src/shared/user/user.module.ts
@@ -1,12 +1,24 @@
 import { Module } from "@nestjs/common";
 
 import { AuthModule } from "../auth/auth.module";
+import { UserAddressModule } from "../user.address/user-address.module";
+import { UserContactsModule } from "../user.contacts/user-contacts.module";
 
 import { UserService } from "./user.service";
 import { UserRepositoryProvider } from "./user.provider";
 import { UserController } from "./user.controller";
+import { UserDetailsModule } from "../user.details/user-details.module";
+import { UserGuardianModule } from "../user.guardian/user-guardian.module";
+import { UserPreviousSchool } from "../database/models/UserPreviousSchool.entity";
 
 @Module({
+  imports: [
+    UserAddressModule,
+    UserContactsModule,
+    UserDetailsModule,
+    UserGuardianModule,
+    UserPreviousSchool,
+  ],
   providers: [UserService, UserRepositoryProvider],
   exports: [UserService],
   controllers: [UserController],
