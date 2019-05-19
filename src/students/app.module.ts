852f944bb52fa9746b5aab8e5517059255180523
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-05-19 12:28:37 +0800

    feat(app-module): removed useraddress module

--- src/students/app.module.ts
+++ src/students/app.module.ts
@@ -3,13 +3,11 @@ import { Module } from "@nestjs/common";
 import { UserModule } from "../shared/user/user.module";
 import { DatabaseModule } from "../shared/database/database.module";
 import { AuthModule } from "../shared/auth/auth.module";
-import { UserAddressModule } from "../shared/user.address/user-address.module";
 
 @Module({
   imports: [
     DatabaseModule,
     UserModule,
-    UserAddressModule,
     AuthModule,
   ],
 })
