aa1e877114532642a373e48a86ee496383d82310
Renz Christen Yeomer A. Pagulayan <rcyapagulayan@gmail.com>
2019-05-22 23:51:29 +0800

    refactor(user): findOne queries with @UserEntity

--- src/shared/user/dto/create-user.dto.ts
+++ src/shared/user/dto/create-user.dto.ts
@@ -4,6 +4,5 @@ export class CreateUserDto implements IUser {
   public readonly enrolledProgram?: string;
   public readonly username: string;
   public readonly password: string;
-  public readonly userId?: string;
   public readonly type: UserType;
 }
--- src/shared/user/user.controller.ts
+++ src/shared/user/user.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Delete, Post, Put, Inject, Body, UnprocessableEntityException, UseGuards } from "@nestjs/common";
-import { Op } from "sequelize";
 import { AuthGuard } from "@nestjs/passport";
+
+import { UserEntity } from "../decorators/user-entity.decorator";
+import { User } from "../database/models/User.entity";
 
 import { UserService } from "./user.service";
 import { CreateUserDto } from "./dto/create-user.dto";
@@ -19,21 +21,15 @@ export class UserController {
 
   @Put()
   @UseGuards(AuthGuard("bearer"))
-  public async updateUser(@Body() createUserDto: CreateUserDto) {
-    const user = await this.userService.findOneById(createUserDto.userId);
-    Object.keys(createUserDto).forEach((key) => {
-      if (key !== "userId") {
-        user[key] = createUserDto[key];
-      }
-    });
+  public async updateUser(@Body() createUserDto: CreateUserDto, @UserEntity() user: User) {
+    Object.keys(createUserDto).forEach((key) => user[key] = createUserDto[key]);
     await user.save();
     return {iat: Date.now()};
   }
 
   @Delete()
   @UseGuards(AuthGuard("bearer"))
-  public async deleteUser(@Body() createUserDto: CreateUserDto) {
-    const user = await this.userService.findOneById(createUserDto.userId);
+  public async deleteUser(@UserEntity() user: User) {
     await user.destroy();
     return {iat: Date.now()};
   }
