6993c6d7bf1b04e9c803a2042cc5e87f29581548
Renz Christen Yeomer A. Pagulayan <rcyapagulayan@gmail.com>
2019-05-22 22:33:50 +0800

    feat(app): import enrollment module

--- src/students/app.module.ts
+++ src/students/app.module.ts
@@ -3,12 +3,14 @@ import { Module } from "@nestjs/common";
 import { UserModule } from "../shared/user/user.module";
 import { DatabaseModule } from "../shared/database/database.module";
 import { AuthModule } from "../shared/auth/auth.module";
+import { EnrollmentModule } from "../shared/enrollment/enrollment.module";
 
 @Module({
   imports: [
     DatabaseModule,
     UserModule,
     AuthModule,
+    EnrollmentModule,
   ],
 })
 export class AppModule {}
